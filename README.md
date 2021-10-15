e8310ed52206c5cb5e520778c0056006381b98b7
Konstantin Tabere <tabere@ubique.ch>
2021-10-15 12:02:46 +0200

    first commit
    Create README.md
    
    Update README.md
    
    Update README.md
    
    Update README.md

---
+++ README.md
@@ -0,0 +1,5 @@
+# webview-changing-colors-bug
+Demonstration of calling requestFocusFromTouch() in WebView is changing colors in Android app
+
+
+![Screenshot_20211015-120734_WebView Colors Bug](https://user-images.githubusercontent.com/43139081/137471467-d270487d-7745-4794-9bb4-6aa34e24ab28.jpg)
---
+++ app/src/main/java/tabere/webview/colors/bug/MainActivity.kt
@@ -0,0 +1,71 @@
+package tabere.webview.colors.bug
+
+import android.annotation.SuppressLint
+import android.app.Dialog
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.LinearLayout
+import tabere.webview.colors.bug.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private var doCallRequestFromTouch = true
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        ActivityMainBinding.inflate(layoutInflater).apply {
+            setContentView(root)
+
+            openWebView.setOnClickListener {
+                openWebViewDialog()
+            }
+
+            radioGroup.setOnCheckedChangeListener { _, checkedId ->
+                doCallRequestFromTouch = checkedId == R.id.doRequestFromTrouch
+            }
+        }
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    private fun openWebViewDialog() {
+        val dialog = Dialog(this, R.style.Theme_WebViewColorsBug).apply {
+            requestWindowFeature(1)
+        }
+        WebView(this).apply {
+            layoutParams = LinearLayout.LayoutParams(-1, -1)
+        }.also {
+
+            it.settings.apply {
+                javaScriptEnabled = true
+                setSupportZoom(true)
+                builtInZoomControls = true
+                displayZoomControls = false
+            }
+
+            it.webViewClient = object : WebViewClient() {
+
+                override fun onPageFinished(view: WebView, url: String) {
+                    if (doCallRequestFromTouch) {
+                        it.requestFocusFromTouch()
+                    }
+                }
+            }
+            it.webChromeClient = WebChromeClient()
+
+            dialog.setContentView(it)
+            dialog.show()
+
+            it.loadUrl("https://www.blank.org")
+
+            Handler(Looper.getMainLooper()).postDelayed({
+                dialog.dismiss()
+            }, 1000)
+        }
+    }
+}
