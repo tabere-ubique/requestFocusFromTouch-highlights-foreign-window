985abae7d9036cf214ce14ac3a619c503527cf0f
Konstantin Tabere <tabere@ubique.ch>
2021-10-18 12:00:29 +0200

    simplification

--- README.md
+++ README.md
@@ -1,20 +1,2 @@
 # webview-changing-colors-bug
-Demonstration of calling requestFocusFromTouch() in WebView is changing colors in Android app
-
-
-![Screenshot_20211015-120734_WebView Colors Bug](https://user-images.githubusercontent.com/43139081/137471467-d270487d-7745-4794-9bb4-6aa34e24ab28.jpg)
-
-This is the problematic code:
-```
- it.webViewClient = object : WebViewClient() {
-
-    override fun onPageFinished(view: WebView, url: String) {
-        /* The problem is with this code here. Calling requestFocusFromTouch()
-        * seems to be propagating the focusing to the MainActivity.
-        * We should not use it! */
-        if (doCallRequestFromTouch) {
-            it.requestFocusFromTouch()
-        }
-    }
-}
-```
+Demonstration of calling requestFocusFromTouch() in dialog is changing colors in foreign window
--- app/src/main/java/tabere/webview/colors/bug/MainActivity.kt
+++ app/src/main/java/tabere/webview/colors/bug/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.View
 import android.view.ViewGroup
 import android.view.Window
 import android.webkit.WebChromeClient
@@ -24,8 +25,8 @@ class MainActivity : AppCompatActivity() {
         ActivityMainBinding.inflate(layoutInflater).apply {
             setContentView(root)
 
-            openWebView.setOnClickListener {
-                openWebViewDialog()
+            openDialog.setOnClickListener {
+                openDialog()
             }
 
             radioGroup.setOnCheckedChangeListener { _, checkedId ->
@@ -34,43 +35,24 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    @SuppressLint("SetJavaScriptEnabled")
-    private fun openWebViewDialog() {
-        val dialog = Dialog(this, R.style.Theme_WebViewColorsBug).apply {
-            requestWindowFeature(Window.FEATURE_NO_TITLE)
-        }
-        WebView(this).apply {
-            layoutParams = ViewGroup.LayoutParams(-1, -1)
+    private fun openDialog() {
+        val dialog = Dialog(this, R.style.Theme_WebViewColorsBug)
+        View(this).apply {
+            layoutParams = ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
         }.also {
-
-            it.settings.apply {
-                javaScriptEnabled = true
-                setSupportZoom(true)
-                builtInZoomControls = true
-                displayZoomControls = false
-            }
-
-            it.webViewClient = object : WebViewClient() {
-
-                override fun onPageFinished(view: WebView, url: String) {
-                    /* The problem is with this code here. Calling requestFocusFromTouch()
-                    * seems to be propagating the focusing to the MainActivity.
-                    * We should not use it! */
-                    if (doCallRequestFromTouch) {
-                        it.requestFocusFromTouch()
-                    }
-                }
-            }
-            it.webChromeClient = WebChromeClient()
-
             dialog.setContentView(it)
             dialog.show()
 
-            it.loadUrl("https://www.blank.org")
-
-            Handler(Looper.getMainLooper()).postDelayed({
-                dialog.dismiss()
-            }, 1000)
+            Handler(Looper.getMainLooper()).apply {
+                postDelayed({
+                    if (doCallRequestFromTouch) {
+                        it.requestFocusFromTouch()
+                    }
+                }, 500)
+                postDelayed({
+                    dialog.dismiss()
+                }, 1000)
+            }
         }
     }
 }
